d36ba2b7d708e856ff7c6fd8445515ea1af79f49
Blain Masaku Muema <62080362+xblainm@users.noreply.github.com>
2022-02-18 09:57:49 +0300

    Making decisions in python

---
+++ main.py
@@ -0,0 +1,13 @@
+﻿a = str(input("Enter string input: "))
+A = ("Spathiphyllum")
+B = ("spathiphyllum")
+
+if a == A:
+    print("Yes - Spathiphyllum is the best plant ever!")
+elif a == B:
+    print("No, I want a big Spathiphyllum!")
+else: 
+    print("Spathiphyllum! Not {}!".format(a))
+    print("Spathiphyllum! Not ", a,"!", sep="")
+    
+    
