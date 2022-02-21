6efd3f6adaa44ad4a76502f25021b3c8e4e12354
Blain Masaku Muema <62080362+xblainm@users.noreply.github.com>
2022-02-21 13:23:26 +0300

    Description for Looping

---
+++ Looping#2.py
@@ -0,0 +1,21 @@
+﻿secret_number = 777
+
+number = int(input(
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+"""))
+
+while number != secret_number:
+    print("Ha ha! You're stuck in my loop!")
+    
+    number = int(input("Enter secret number again: "))
+
+print("Well done, muggle! You are free now.")
+
+
